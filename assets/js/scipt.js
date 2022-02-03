172c1521c4ee1c88c067e0be1c0b46072556cf4b
DavidHearl <david.hearl@outlook.com>
2022-02-03 12:23:54 +0000

    added randomness to second board

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,10 +1,34 @@
-document.getElementById('body').addEventListener('keydown', e => {
-    $("#" + e.key).css("background", "black")
-    $("#typed-text").text(e.key)
-});
+const log = $('body');
+document.addEventListener('keydown', function1);
+document.addEventListener('keyup', function2);
+
+// key = letter
+// code = key'letter'
+// keyCode = unicode
 
 
-document.getElementById('body').addEventListener('keyup', e => {
-    $("#" + e.key).css("background", "orange")
-    $("#changed-text").text(e.key)
-});
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+function function1(e) {
+
+    var random = randomInt(65, 90);
+    $("#" + e.key).css("background", "black");
+    $("#typed-text").text(e.key);
+    $("#changed-text").text(random);
+    $("#" + random).css("background", "black");
+
+
+}
+
+function function2(e, random) {
+
+    $("#" + e.key).css("background", "orange");
+    for (let i = 65; i < 91; i++) {
+        $("#" + i).css("background", "blueviolet");
+    }
+}
